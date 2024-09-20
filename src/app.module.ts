114982c6167740adb1ee69280a9a553ff095158b
xuankha1011 <nguyenxuankha5371@gmail.com>
2024-09-20 17:10:43 +0700

    update ATGuard for authen

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,23 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
-import { PassportModule } from '@nestjs/passport';
-import { IngredientModule } from './ingredient/ingredient.module';
-import { CustomDishModule } from './custom-dish/custom-dish.module';
-import { CustomDishIngredientModule } from './custom-dish-ingredient/custom-dish-ingredient.module';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { APP_GUARD } from '@nestjs/core'
+import { PassportModule } from '@nestjs/passport'
+import { IngredientModule } from './ingredient/ingredient.module'
+import { CustomDishModule } from './custom-dish/custom-dish.module'
+import { CustomDishIngredientModule } from './custom-dish-ingredient/custom-dish-ingredient.module'
 
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { AuthModule } from './auth/auth.module';
-import { ComboItemModule } from './combo-item/combo-item.module';
-import { ComboModule } from './combo/combo.module';
-import { CustomerModule } from './customer/customer.module';
-import { FoodModule } from './food/food.module';
-import { OrderDetailModule } from './order-detail/order-detail.module';
-import { OrderModule } from './order/order.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { ReviewModule } from './review/review.module';
-import { TransactionModule } from './transaction/transaction.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
+import { AuthModule } from './auth/auth.module'
+import { ComboItemModule } from './combo-item/combo-item.module'
+import { ComboModule } from './combo/combo.module'
+import { CustomerModule } from './customer/customer.module'
+import { FoodModule } from './food/food.module'
+import { OrderDetailModule } from './order-detail/order-detail.module'
+import { OrderModule } from './order/order.module'
+import { PrismaModule } from './prisma/prisma.module'
+import { ReviewModule } from './review/review.module'
+import { TransactionModule } from './transaction/transaction.module'
+import { AtGuard } from './common/guards'
 
 @Module({
   imports: [
@@ -55,6 +56,10 @@ import { TransactionModule } from './transaction/transaction.module';
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
     },
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
+    },
   ],
 })
-export class AppModule {}
+export class AppModule { }
